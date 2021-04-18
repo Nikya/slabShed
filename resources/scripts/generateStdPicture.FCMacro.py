7ffae734426d5117057355e4e7580fe3fb09410c
Nikya <severac.yannick@gmail.com>
2021-04-18 17:11:03 +0200

    Improve picture export Macro

--- resources/scripts/generateStdPicture.FCMacro.py
+++ resources/scripts/generateStdPicture.FCMacro.py
@@ -1,5 +1,4 @@
 """
-
 	Part of Slab Shed Project 
 	
 	To export a standard picture of a Slab or a Shed from FreeCad
@@ -9,30 +8,35 @@
 	- Select the object to export in the construction tree
 	- Execute this macro
 	Result: 
-	- 2 pictures are available into the parent folder of te FreeCad file
+	- 2 pictures are available into the parent/resources folder of te FreeCad file
 	- In recomanded size : 480x360 4:3
-	- Now keep only expected file and rename it (See CONTRIBUTING file)
+	- Now keep only expected file (Slab or Shed) and rename it (See CONTRIBUTING file)
 """
 
 import FreeCAD
 
-# 2 possible output files
-filePathSlab = '../slab_renameMeColl_slabName.png'
-filePathShed = '../shed_99_88x77_renameMeColl_shedName.png'
-
-# Others
+#####  OUTPUT FILES PROPERTIES
+#  2 possible output files names
+filePathSlab = '../resources/slab_myNewCollectionName_mySlabName.png'
+filePathShed = '../resources/shed_99_88x77_myNewCollectionName_myShedName.png'
 fileComment = 'Part of the Slab Shed Project\nhttps://github.com/Nikya/slabShed'
+fileWidth=480
+fileHight=360
 
-# Set 3D view to Isometric
+##### FRAMING & DISPLAY
+# Set 3D view to Isometric+Orthographic
 Gui.activeDocument().activeView().viewIsometric()
-
+Gui.activeDocument().activeView().setCameraType("Orthographic")
 # Fit Zoom to selected Part object
 FreeCADGui.SendMsgToActiveView('ViewSelection')
+# TODO : Change background color, change shape color, then restore color
+# Now unselect all
+Gui.Selection.clearSelection()
 
-# Save 2 files 
-Gui.activeDocument().activeView().saveImage(filePathSlab,480,360,'Current',fileComment)
-Gui.activeDocument().activeView().saveImage(filePathShed,480,360,'Current',fileComment)
+##### GENERATE
+Gui.activeDocument().activeView().saveImage(filePathSlab, fileWidth, fileHight, 'Current', fileComment)
+Gui.activeDocument().activeView().saveImage(filePathShed, fileWidth, fileHight, 'Current', fileComment)
 
-# It's done!
+# DONE!
 print("File exported " + filePathSlab)
 print("File exported " + filePathShed)
